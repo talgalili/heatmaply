0c9e4157e93649045e35a64c12d72944b04d4a73
Alanocallaghan <alan.ocallaghan@outlook.com>
2017-04-12 20:24:33 +0100

    Update plots.R

--- R/plots.R
+++ R/plots.R
@@ -153,8 +153,8 @@ ggplot_heatmap <- function(xx,
 plotly_heatmap <- function(x, limits = range(x), colors = viridis(n=256, alpha = 1, begin = 0,
                                                                   end = 1, option = "viridis"),
     row_text_angle = 0, column_text_angle = 45, grid.color, grid.size, key.title = NULL,
-    row_dend_left = FALSE, fontsize_row = 10, fontsize_col = 10, colorbar_xanchor = 1, 
-    colorbar_yanchor = 1, colorbar_xpos = 1, colorbar_ypos = 1, colorbar_len = 0.3) {
+    row_dend_left = FALSE, fontsize_row = 10, fontsize_col = 10, colorbar_xanchor = "middle", 
+    colorbar_yanchor = "middle", colorbar_xpos = 1, colorbar_ypos = 1, colorbar_len = 0.3) {
 
   p <- plot_ly(z = x, x = 1:ncol(x), y = 1:nrow(x),
     type = "heatmap", showlegend = FALSE, colors = colors,
