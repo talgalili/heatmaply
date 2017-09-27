55ec9e143dba6c12d9e715081f0ba0ff605e5633
Tal Galili <tal.galili@gmail.com>
2017-09-27 05:02:21 +0300

    explicit reference internal functions so to allow test_dir to work

--- tests/testthat/test_plots.R
+++ tests/testthat/test_plots.R
@@ -6,10 +6,10 @@ iris_category <- iris[, 5, drop = FALSE]
 test_that("ggplot_heatmap works", {
 	g <- heatmaply:::ggplot_heatmap(as.matrix(iris_plot))
 	expect_is(g, "ggplot")
-    g <- heatmaply:::ggplot_heatmap(as.matrix(iris_plot), 
+    g <- heatmaply:::ggplot_heatmap(as.matrix(iris_plot),
         label_names=c("a", "b", "c"))
     expect_is(g, "ggplot")
-    expect_error(heatmaply:::ggplot_heatmap(as.matrix(iris_plot), 
+    expect_error(heatmaply:::ggplot_heatmap(as.matrix(iris_plot),
         label_names=c("a")))
 })
 
@@ -25,22 +25,22 @@ test_that("plotly_dend works", {
 })
 
 test_that("ggplot_side_color_plot works", {
-	g <- heatmaply:::ggplot_side_color_plot(iris_category, 
+	g <- heatmaply:::ggplot_side_color_plot(iris_category,
 		type = "row")
 	expect_is(g, "ggplot")
 	expect_error(ggplotly(g), NA)
-	g <- heatmaply:::ggplot_side_color_plot(iris_category, 
+	g <- heatmaply:::ggplot_side_color_plot(iris_category,
 		type = "column")
 	expect_is(g, "ggplot")
 	expect_error(ggplotly(g), NA)
 })
 
 test_that("plotly_side_color_plot works", {
-	p <- heatmaply:::plotly_side_color_plot(iris_category, 
+	p <- heatmaply:::plotly_side_color_plot(iris_category,
 		type = "row")
 	expect_is(p, "plotly")
 	expect_error(ggplotly(p), NA)
-	p <- heatmaply:::plotly_side_color_plot(iris_category, 
+	p <- heatmaply:::plotly_side_color_plot(iris_category,
 		type = "column")
 	expect_is(p, "plotly")
 	expect_error(ggplotly(p), NA)
@@ -51,10 +51,10 @@ test_that("predict_colors works", {
 	p <- heatmaply:::plotly_heatmap(as.matrix(iris_plot))
 	g <- heatmaply:::ggplot_heatmap(as.matrix(iris_plot))
 	expect_error(
-        heatmaply:::predict_colors(p, plot_method = "plotly"), 
+        heatmaply:::predict_colors(p, plot_method = "plotly"),
         NA)
 	expect_error(
-		heatmaply:::predict_colors(ggplotly(g), plot_method = "ggplot"), 
+		heatmaply:::predict_colors(ggplotly(g), plot_method = "ggplot"),
 		NA)
 })
 
@@ -75,8 +75,8 @@ test_that("parse_plotly_color works", {
 })
 
 test_that("k_colors works", {
-	expect_equal(k_colors(1), "black")
-	expect_equal(k_colors(5), colorspace::rainbow_hcl(5))
+	expect_equal(heatmaply:::k_colors(1), "black")
+	expect_equal(heatmaply:::k_colors(5), colorspace::rainbow_hcl(5))
 })
 
 test_that("default colors works", {
@@ -88,16 +88,16 @@ test_that("default colors works", {
 test_that("numeric cols on dendrograms", {
     dend <- as.dendrogram(hclust(dist(mtcars)))
     dend <- dendextend::set(dend, "branches_k_color", 1:32)
-    expect_silent(plotly_dend(dend))
+    expect_silent(heatmaply:::plotly_dend(dend))
 })
 
 test_that("side colors fail when bad palette", {
     expect_error(
-        heatmaply:::ggplot_side_color_plot(iris[, 5, drop=FALSE]), 
+        heatmaply:::ggplot_side_color_plot(iris[, 5, drop=FALSE]),
         NA)
     expect_error(
         heatmaply:::ggplot_side_color_plot(iris[, 5, drop=FALSE],
-            palette = setNames(RColorBrewer::brewer.pal(3, "Set3"), 
+            palette = setNames(RColorBrewer::brewer.pal(3, "Set3"),
                 unique(iris[, 5]))),
         NA)
     expect_error(
@@ -105,11 +105,11 @@ test_that("side colors fail when bad palette", {
             palette = c("Nope" = "Nope")))
 
     expect_error(
-        heatmaply:::plotly_side_color_plot(iris[, 5, drop=FALSE]), 
+        heatmaply:::plotly_side_color_plot(iris[, 5, drop=FALSE]),
         NA)
     expect_error(
         heatmaply:::plotly_side_color_plot(iris[, 5, drop=FALSE],
-            palette = setNames(RColorBrewer::brewer.pal(3, "Set3"), 
+            palette = setNames(RColorBrewer::brewer.pal(3, "Set3"),
                 unique(iris[, 5]))),
         NA)
     expect_error(
@@ -120,9 +120,9 @@ test_that("side colors fail when bad palette", {
 test_that("plotly dend uses colors", {
     dend <- hclust(dist(mtcars))
     dend <- dendextend::color_branches(dend, k=2)
-    p <- plotly_dend(dend)
+    p <- heatmaply:::plotly_dend(dend)
     expect_is(p, "plotly")
     dend <- dendextend::color_branches(dend, k=2, col=c(1, 2))
-    p <- plotly_dend(dend)
+    p <- heatmaply:::plotly_dend(dend)
     expect_is(p, "plotly")
 })
