119e3738c740e329a3183c1d079149538f3c2580
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2017-12-19 20:04:30 +0000

    Remove rcorr

--- tests/testthat/test_heatmaply_misc.R
+++ tests/testthat/test_heatmaply_misc.R
@@ -1,11 +1,15 @@
 context("heatmaply misc")
 
 test_that("node argument works", {
-  c <- Hmisc::rcorr(as.matrix(mtcars))
+  rrapply <- function(A, FUN, ...) mapply(function(a, B) lapply(B, 
+           function(x) FUN(a, x, ...)), a = A, MoreArgs = list(B = A))
+  cor.tests <- rrapply(mtcars, cor.test) # a matrix of cor.tests
+  p <- apply(cor.tests, 1:2, function(x) x[[1]]$p.value) # and it's there
+  r <- cor(mtcars)
   h <- heatmaply(
-    c$r, node_type = "scatter",
+    r, node_type = "scatter",
     point_size_name = "p",
-    point_size_mat = -log10(c$P)
+    point_size_mat = -log10(p)
   )
 })
 
