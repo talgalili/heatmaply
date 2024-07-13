6ce178e799985082979111d895ddf1cbf5ee5a77
mcsimenc@gmail.com <mcsimenc@gmail.com>
2024-07-13 10:31:51 -0700

    revert if/else switch

--- R/plots.R
+++ R/plots.R
@@ -101,10 +101,10 @@ ggplot_heatmap <- function(xx,
     }
   }
   if (!is.null(custom_hovertext)) {
-    if(suppress_default_hovertext) {
-      mdf[["text"]] <- reshape2::melt(as.matrix(custom_hovertext))[[3]]
-    } else {
+    if(!suppress_default_hovertext) {
       mdf[["text"]] <- paste0(mdf[["text"]], "<br>", custom_hovertext)
+    } else {
+      mdf[["text"]] <- reshape2::melt(as.matrix(custom_hovertext))[[3]]
     }
   }
   geom_args[["mapping"]] <- aes_mapping
