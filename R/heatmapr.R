07a1baaa63f3cc1d2db81068ab56df6873e90e97
mcsimenc@gmail.com <mcsimenc@gmail.com>
2024-07-14 11:49:16 -0700

    Add @param for suppress_default_hovertext to heatmapr

--- R/heatmapr.R
+++ R/heatmapr.R
@@ -126,6 +126,8 @@ fix_not_all_unique <- function(x, ...) {
 #' "GW" (Gruvaeus and Wainer heuristic to optimize the Hamiltonian path length that is restricted by the dendrogram structure)
 #' @param point_size_mat A matrix of values which can be mapped to point size
 #' @param custom_hovertext Custom hovertext matrix (the same dimensions as the input).
+#' @param suppress_default_hovertext Logical value determinine whether to suppress
+#'  the default hovertext (row, column, value).
 #' @param ... currently ignored
 #'
 #' @export
